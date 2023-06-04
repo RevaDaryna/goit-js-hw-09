c296dadb616446a2c221e5f2c6e0342d3ef41cbe
RevaDaryna <revadara32@gmail.com>
2023-06-04 19:11:05 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,35 @@
-import Notiflix from 'notiflix';
-const form = document.querySelector('.form');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-form.addEventListener('submit', e => {
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
   e.preventDefault();
-
-  const delay = parseInt(form.elements.delay.value);
-  const step = parseInt(form.elements.step.value);
-  const amount = parseInt(form.elements.amount.value);
-
-  for (let i = 1; i <= amount; i++) {
-    createPromise(i, delay + i * step)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
+  const delay = refs.form.elements.delay.value;
+  const step = refs.form.elements.step.value;
+  const amount = refs.form.elements.amount.value;
+  for (let i = 1; i <= amount; i += 1) {
+    const currentDelay = Number(delay) + step * (i - 1);
+    createPromise(i, currentDelay)
+      .then(result => {
+        Notify.success(`✅ Fulfilled promise ${i} in ${currentDelay}ms`);
       })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
+      .catch(result => {
+        Notify.failure(`❌ Rejected promise ${i} in ${currentDelay}ms`);
       });
   }
-});
-
+}
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
-        // Fulfill
       } else {
         reject({ position, delay });
-        // Reject
       }
     }, delay);
   });
